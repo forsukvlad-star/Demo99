3585940dd2a2493487058d177abc34c3a55e9e82
forsukvlad-star <forsuk.vlad@gmail.com>
2026-05-07 12:09:06 +0300

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,17 @@
-// function myTask(callback) {
-//   console.log('Hello Vlad!');
-
-//   callback();
-//  }
-
-// async function myTask2() {
-//   console.log('Task2: Hello Vlad!');
-
-//  }
-
-// exports.myTask = myTask;
-// exports.myTask2 = myTask2;
-
-// exports.myTask3 = async function () {
-    // console.log('Task3: Hello Vlad!');
-// }
-
-// exports.myTask4 = async() => {
-    // console.log('Task4: Hello Vlad HPM!');
-// }
-
 const gulp = require('gulp');
+const cleanCSS = require('gulp-clean-css');
+const uglify = require('gulp-uglify');
 
-exports.cloneFiles = async () => {
- return gulp.src('app/*.html') //копіювання всіх html файлів з папки app
- .pipe(gulp.dest('public')); //виведення в public
-}
+// Задача для CSS
+exports.minifyCSS = async () => {
+    return gulp.src('app/**/*.css')
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('public'));
+};
+
+// Задача для JS
+exports.minifyJS = async () => {
+    return gulp.src('app/**/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('public'));
+};
